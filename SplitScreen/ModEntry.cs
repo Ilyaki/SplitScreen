c8a7b6562c6672d2da481bd66c4f56a2a13349bc
Ilyaki <ilyaki.ilyaki@gmail.com>
2018-05-11 18:15:43 +0100

    2.0.0 Update: > Added new launch option --player-index X (to simplify setup) > Fixed jittery mouse movement > Fixed ShippingMenu (after sleeping) not responsive on inactive windows > Added toaster notification if windows are setup improperly

--- SplitScreen/ModEntry.cs
+++ SplitScreen/ModEntry.cs
@@ -18,7 +18,7 @@ using StardewValley.Menus;
  *	- UpdateControlInput is called when it should according to SDV's Game1 if statement logic, except ONLY when InActive
  *         >same with updateActiveMenu
  */
- 
+
 namespace SplitScreen
 {
 	public class ModEntry : Mod
@@ -27,32 +27,84 @@ namespace SplitScreen
 
 		private InputState sInputState;
 
+		private PlayerIndexController playerIndexController;
+
+		private bool hasShippingMenuCompleted = false;
+
+		private const int secondsBetweenWarnings = 5;
+
 		public override void Entry(IModHelper helper)
 		{
 			this.smapiHelper = helper;
 
+			//Get player index if it is set in launch options, e.g. SMAPI.exe --log-path "third.txt" --player-index 3
+			playerIndexController = new PlayerIndexController(Monitor, Environment.GetCommandLineArgs());
+			
 			//Removes FPS throttle when window inactive
 			Game1.game1.InactiveSleepTime = new TimeSpan(0);
 
 			/* When Program.releaseBuild = false, LeftStick and RightStick open a performance dialog and the chat window, respectively
-			* So it is a good idea to unbind LeftStick and RightStick in x360ce */
-			//Prevents not continuing with update threads when InActive
+			   So it is a good idea to unbind LeftStick and RightStick in x360ce */
+			/* Prevents not continuing with update threads when InActive 
+			  Also calls updateActiveMenu when window is inactive so we don't have to */
 			Program.releaseBuild = false;
 
 			//SMAPI sets Game1.input to an instance of SInputState
 			sInputState = (smapiHelper.Reflection.GetField<InputState>(typeof(Game1), "input", true)).GetValue();
 
 			//Manually update the RealController and RealMouse fields of SMAPI's SInputeState (which derives from SDV's InputState but is internal and sealed, so needs reflection)
+			//Also manually call Game1.UpdateControlInput
 			StardewModdingAPI.Events.GameEvents.UpdateTick += OnUpdateTick;
+
+			//Only used for manually closing Shipping Menu for non-Host players
+			StardewModdingAPI.Events.SpecialisedEvents.UnvalidatedUpdateTick += OnUnvalidatedUpdateTick;
+			StardewModdingAPI.Events.SaveEvents.AfterSave += (o, e) => { hasShippingMenuCompleted = false; };
+
+			//Used to send (Toaster notification) warnings periodically if windows are set up incorrectly
+			int secondsPassed = 0;
+			StardewModdingAPI.Events.GameEvents.OneSecondTick += (o, e) =>
+			{
+				secondsPassed = ++secondsPassed % secondsBetweenWarnings;
+				if(secondsPassed == 0)
+				{
+					/* playerIndex 1 should be active window */
+					if (Context.IsMultiplayer && playerIndexController.isPlayerIndexEqual(PlayerIndex.One) && !Game1.game1.IsActive)
+						Game1.showGlobalMessage("Error: Controller index One's window must be active");
+					/*else if (Context.IsMultiplayer && Context.IsMainPlayer && !playerIndexController.isPlayerIndexEqual(PlayerIndex.One))
+						Game1.showGlobalMessage($"Error: Host must have controller index One (Currently is {playerIndexController.getIndexAsString()})"); (actually seems fine) */
+
+				}
+			};
+		}
+		
+		private void OnUnvalidatedUpdateTick (object sender, EventArgs e)
+		{
+			/*From testing, it seems that between Saving (ie between SaveEvents.BeforeSave and SaveEvents.AfterSave), 
+			  GameEvents.UpdateTick is not called. SpecializedEvents.UnvalidatedUpdateTick is always called, however.
+			  For some (still unknown..) reason, ShippingMenu is unresponsive during save period when window is inactive
+			  Workaround: Wait for host to finish the menu, then manually call the OK button (which closes the menu)*/
+			  
+			if (!hasShippingMenuCompleted && !Game1.game1.IsActive && Game1.activeClickableMenu != null && Game1.activeClickableMenu is ShippingMenu shippingMenu)
+			{
+				if (Game1.player.team.GetNumberReady("wakeup") > 0)//This will equal 1 once host has finished with ShippingMenu
+				{
+					Monitor.Log("Manually closing ShippingMenu", LogLevel.Trace);
+					hasShippingMenuCompleted = true;
+					shippingMenu.InvokeMethod("okClicked");//Simulates menu close
+				}
+				//shippingMenu.update(Game1.currentGameTime); TODO: uncomment
+			}
 		}
 
-		private void OnUpdateTick (object sender, EventArgs e)
+		private void OnUpdateTick(object sender, EventArgs e)
 		{
 			#region Insert raw GamePadState/MouseState to SInputState
 			try
 			{
-				GamePadState rawGamePadState = GamePad.GetState(PlayerIndex.One);
+				//If no playerIndex is set, a blank input state will be passed in
+				GamePadState rawGamePadState = playerIndexController.GetRawGamePadState();
 				sInputState.SetPrivatePropertyValue("RealController", rawGamePadState);
+				sInputState.SetPrivatePropertyValue("SuppressedController", rawGamePadState);
 
 				#region Passing in MouseState
 				if (!Game1.game1.IsActive)//Otherwise breaks mouse input for active window
@@ -66,9 +118,13 @@ namespace SplitScreen
 					MouseState rawMouseState = Mouse.GetState();
 					MouseState artificialMouseState = new MouseState(relativeMousePos.X, relativeMousePos.Y,
 						0, ButtonState.Released, ButtonState.Released, ButtonState.Released, ButtonState.Released, ButtonState.Released);
-					//TODO: does scroll wheel value 0 break menus?
+
+					Point relativeMousePosWithZoom = new Point((int)(relativeMousePos.X * (1.0 / Game1.options.zoomLevel)), (int)(relativeMousePos.Y * (1.0 / Game1.options.zoomLevel)));
+
 
 					sInputState.SetPrivatePropertyValue("RealMouse", artificialMouseState);
+					sInputState.SetPrivatePropertyValue("MousePosition", relativeMousePosWithZoom);//These last 2 lines fixed jittery mouse when moving either sticks AND fixed jittery mouse in menus
+					sInputState.SetPrivatePropertyValue("SuppressedMouse", artificialMouseState);
 				}
 				#endregion
 			}
@@ -80,15 +136,14 @@ namespace SplitScreen
 
 			#region Manually call UpdateControlInput
 			if (
-				(!Game1.game1.IsSaving
-				&& Game1.gameMode != 11
-				&& (Game1.gameMode == 2 || Game1.gameMode == 3)
+				(!Game1.game1.IsSaving 
+				&& Game1.gameMode != 11 && (Game1.gameMode == 2 || Game1.gameMode == 3)
 				&& Game1.currentLocation != null
-				//&& Game1.currentMinigame == null
+				&& Game1.currentMinigame == null
 				&& Game1.activeClickableMenu == null
 				&& !Game1.globalFade
 				&& !Game1.freezeControls
-				&& !Game1.game1.IsActive) // so its the opposite
+				&& !Game1.game1.IsActive)
 
 			|| (Game1.farmEvent == null
 				&& Game1.dialogueUp == true
@@ -104,10 +159,11 @@ namespace SplitScreen
 			}
 			#endregion
 
+			/*Unnecessary as Program.releaseBuild = false will make SDV call this for us when the window is inactive!
 			#region Manually call UpdateActiveMenu
 			if (//Game1._newDayTask == null && 
-				!Game1.game1.IsSaving
-				&& (Game1.gameMode == 2 || Game1.gameMode == 3) && (Game1.gameMode != 11)
+				!Game1.game1.IsSaving &&
+				 (Game1.gameMode == 2 || Game1.gameMode == 3) && (Game1.gameMode != 11)
 				&& Game1.currentLocation != null && Game1.currentMinigame == null
 				&& Game1.activeClickableMenu != null
 				&& !Game1.game1.IsActive
@@ -115,7 +171,7 @@ namespace SplitScreen
 			{
 				Game1.updateActiveMenu(Game1.currentGameTime);
 			}
-			#endregion
-		}
+			#endregion*/
+		}		
 	}
 }
---
+++ SplitScreen/PlayerIndexController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplitScreen
+{
+	class PlayerIndexController
+	{
+		private IMonitor monitor;
+
+		private PlayerIndex? playerIndex;
+
+		public PlayerIndexController(IMonitor monitor, string[] args)
+		{
+			this.monitor = monitor;
+
+			playerIndex = GetPlayerIndexFromArgs(args);
+			monitor.Log($"Using player index {getIndexAsString()}", LogLevel.Info);
+		}
+
+		public GamePadState GetRawGamePadState() => playerIndex.HasValue ? GamePad.GetState(playerIndex.GetValueOrDefault()) : new GamePadState(new Vector2(), new Vector2(), 0, 0);
+		
+		public bool isPlayerIndexEqual(PlayerIndex playerIndex) => this.playerIndex.HasValue && this.playerIndex.Equals(playerIndex);
+
+		public string getIndexAsString() => playerIndex.HasValue ? playerIndex.ToString() : "NONE";
+
+		private PlayerIndex? GetPlayerIndexFromArgs(string[] args)
+		{
+			if (args.Contains("--player-index"))
+			{
+				int playerIndex_Index = Array.LastIndexOf(args, "--player-index") + 1;
+				if (playerIndex_Index >= 1 && args.Length >= playerIndex_Index)
+				{
+					string playerIndexString = args[playerIndex_Index];
+					switch (playerIndexString)
+					{
+						case "0":
+						case "zero":
+						case "none":
+						case "null":
+							return null;
+						case "1":
+						case "one":
+						case "first":
+							return PlayerIndex.One;
+						case "2":
+						case "two":
+						case "second":
+							return PlayerIndex.Two;
+						case "3":
+						case "three":
+						case "third":
+							return PlayerIndex.Three;
+						case "4":
+						case "four":
+						case "fourth":
+							return PlayerIndex.Four;
+						default:
+							monitor.Log("'--player-index' launch option set incorrectly", LogLevel.Error);
+							return null;
+					}
+				}
+				else
+				{
+					monitor.Log("'--player-index' launch option set incorrectly", LogLevel.Error);
+				}
+			}
+
+			return PlayerIndex.One;
+		}
+	}
+}
